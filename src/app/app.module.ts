a8b867c1e5d33918affb3de9cbec6bdc1cd2e764
NguyenThiLinhCNTT <70678670+NguyenThiLinhCNTT@users.noreply.github.com>
2020-09-30 16:09:37 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule
